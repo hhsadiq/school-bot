717d6da3d6c9afc06cbfd7bc0e3fa310cce707d9
hhsadiq <hhsadiq@gmail.com>
2017-07-19 13:11:49 +0500

    validation code

--- controllers/bot.js
+++ controllers/bot.js
@@ -3,7 +3,12 @@
  * Home page.
  */
 exports.index = (req, res) => {
-  res.render('bot', {
-    title: 'Bot'
-  });
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === process.env.FACEBOOK_MESSENGER_BOT_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
 };
