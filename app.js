8e908d7cd497c4369b2ed0dd858370c917c5ebd8
hhsadiq <hhsadiq@gmail.com>
2017-07-19 17:40:53 +0500

    Exclude bot route

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use((req, res, next) => {
-  if (req.path === '/api/upload') {
+  if (req.path === '/api/upload' || req.path === '/bot') {
     next();
   } else {
     lusca.csrf()(req, res, next);
